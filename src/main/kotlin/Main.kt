e23ba4f78d75ecb40a5ee9a9a0eaeda9f739d3ae
Johannes Löhnn <jlohnn@gmail.com>
2018-08-09 15:47:11 +0200

    Support for reading input from and writing output to files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.SystemExitException
+import java.io.File
 import java.io.OutputStreamWriter
 import kotlin.system.exitProcess
 
@@ -15,8 +16,33 @@ fun main(args: Array<String>) {
         ParsedArgs(parser).run {
             parser.force()
 
-            val inputList = jackson.readValue<InputList>(input)
-            println(jackson.writeValueAsString(inputList.rank()))
+            if (validate) {
+                val inputList = jackson.readValue<InputList>(inputFile!!.readText())
+                val outputRank = jackson.readValue<Result>(outputFile!!.readText())
+
+                if (inputList.rank() == outputRank) {
+                    println("True")
+                    exitProcess(0)
+                } else {
+                    println("False")
+                    exitProcess(1)
+                }
+            } else {
+                if (input != null && inputFile != null) {
+                    println("Decide wether to use input or input file...")
+                    exitProcess(1)
+                }
+                if (input == null && inputFile == null) {
+                    println("You have to have one of input or input file...")
+                    exitProcess(1)
+                }
+
+                getInput(input, inputFile).let {
+                    val inputList = jackson.readValue<InputList>(it)
+                    output(outputFile) { jackson.writeValueAsString(inputList.rank()) }
+                }
+
+            }
         }
     } catch (e: SystemExitException) {
         e.printAndExit()
@@ -25,6 +51,16 @@ fun main(args: Array<String>) {
     } catch (e: IllegalArgumentException) {
         exitMalformedParameter()
     }
+}
+
+fun output(outputFile: File?, function: () -> String) {
+    outputFile?.printWriter()?.use {
+        it.println(function())
+    } ?: run { println(function()) }
+}
+
+fun getInput(input: String?, inputFile: File?): String {
+    return input ?: inputFile?.readText() ?: ""
 }
 
 private fun exitMalformedParameter() {
--- src/main/kotlin/ParsedArgs.kt
+++ src/main/kotlin/ParsedArgs.kt
@@ -1,5 +1,17 @@
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+import java.io.File
 
 class ParsedArgs(parser: ArgParser) {
-    val input by parser.storing(names = "-i", help = "Input your votes here in a json format")
+    val validate by parser.flagging(
+            "-v", "--validate",
+            help = "Validate input and output")
+
+    val inputFile by parser.storing("-s", "--source",
+            help = "Source file") { File(this) }.default<File?>(null)
+
+    val input by parser.storing(names = "-i", help = "Input your votes here in a json format").default<String?>(null)
+
+    val outputFile by parser.storing("-d", "--destination",
+            help = "Destination file") { File(this) }.default<File?>(null)
 }
