8eb43e18a16b45d184604f226756bee3a41450a0
Johannes Löhnn <jlohnn@gmail.com>
2019-01-06 17:47:34 +0100

    Main has some more logging and outputs result to file

--- golang/main.go
+++ golang/main.go
@@ -4,18 +4,28 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"time"
 )
 
 func main() {
+	var start = time.Now()
+	var file = strconv.Itoa(1)
 	var il InputList
-	bytes, e := ioutil.ReadFile("test_files/test1_in.json")
+	bytes, e := ioutil.ReadFile("test_files/test" + file + "_in.json")
 	check(e)
 	json.Unmarshal(bytes, &il)
-	fmt.Println(il)
+	fmt.Println("Finished reading JSON")
 
 	result, e := calc(il)
 	check(e)
 	resultJson, e := json.Marshal(result)
 	check(e)
-	fmt.Println(string(resultJson))
+	e = ioutil.WriteFile("test_files/test"+file+"_out.json", resultJson, 0644)
+	check(e)
+	var elapsed = time.Now().Sub(start)
+	fmt.Println("Operation took " + elapsed.String())
+	fmt.Println()
+	fmt.Println("===================")
+	fmt.Println()
 }
