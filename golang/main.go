9e2e807a3c7dd070f9e88860b5e2b4e2cd1d387c
Johannes Löhnn <jlohnn@gmail.com>
2019-04-19 00:13:26 +0200

    Formatted main a bit

--- golang/main.go
+++ golang/main.go
@@ -12,7 +12,9 @@ func main() {
 	var start = time.Now()
 	var file = strconv.Itoa(1)
 	var il InputList
-	bytes, e := ioutil.ReadFile("test_files/test" + file + "_in.json")
+	var readFile = "test_files/test" + file + "_in.json"
+	fmt.Println("Reading from file " + readFile)
+	bytes, e := ioutil.ReadFile(readFile)
 	check(e)
 	json.Unmarshal(bytes, &il)
 	fmt.Println("Finished reading JSON")
@@ -21,7 +23,9 @@ func main() {
 	check(e)
 	resultJson, e := json.Marshal(result)
 	check(e)
-	e = ioutil.WriteFile("test_files/test"+file+"_out.json", resultJson, 0644)
+	var writeFile = "test_files/test" + file + "_out.json"
+	e = ioutil.WriteFile(writeFile, resultJson, 0644)
+	fmt.Println("Writing to file " + writeFile)
 	check(e)
 	var elapsed = time.Now().Sub(start)
 	fmt.Println("Operation took " + elapsed.String())
